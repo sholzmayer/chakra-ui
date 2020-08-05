b8bce983d1da45959574722f1187f711c7a38043
Gerrit Alex <admin@gerritalex.de>
2020-08-05 23:02:02 +0200

    refactor(test-utils): add testA11y wrapper, extend custom render

--- packages/test-utils/src/test-utils.tsx
+++ packages/test-utils/src/test-utils.tsx
@@ -1,26 +1,76 @@
 import theme from "@chakra-ui/theme"
-import { GlobalStyle, ThemeProvider } from "@chakra-ui/system"
+import { GlobalStyle, ThemeProvider, WithChildren } from "@chakra-ui/system"
 import CSSReset from "@chakra-ui/css-reset"
 import "@testing-library/jest-dom/extend-expect"
-import { render, RenderOptions, fireEvent } from "@testing-library/react"
+import {
+  render as rtlRender,
+  RenderOptions,
+  fireEvent,
+  RenderResult,
+} from "@testing-library/react"
 import * as React from "react"
-import { toHaveNoViolations } from "jest-axe"
+import { toHaveNoViolations, axe } from "jest-axe"
 import serializer from "jest-emotion"
+import { RunOptions } from "axe-core"
 
 expect.addSnapshotSerializer(serializer)
 
 expect.extend(toHaveNoViolations)
 
-const AllProviders: React.FC = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <CSSReset />
-    <GlobalStyle />
-    {children}
-  </ThemeProvider>
-)
+type UI = Parameters<typeof rtlRender>[0]
 
-const customRender = (ui: React.ReactElement, options?: RenderOptions) =>
-  render(ui, { wrapper: AllProviders, ...options })
+// UI-less passthrough fallback to prevent using conditional logic in render
+function ChildrenPassthrough({ children }: { children: React.ReactElement }) {
+  return children
+}
+
+export interface TestOptions extends Omit<RenderOptions, "wrapper"> {
+  /**
+   * optional additional wrapper, e.g. Context
+   *
+   * @example
+   * ```ts
+   * // single wrapper
+   * render(<MyConponent />, {
+   *  wrapper: MyContext
+   * });
+   *
+   * // multiple wrapper
+   * render(<MyConponent />, {
+   *  wrapper: ({ children }) => (
+   *    <ContextA>
+   *      <ContextB>
+   *        {children}
+   *      <ContextB />
+   *    <ContextA />
+   *  )
+   * });
+   *
+   * ```
+   */
+  wrapper?: typeof ChildrenPassthrough
+}
+
+/**
+ * Custom render for @testing-library/react
+ *
+ * @see https://testing-library.com/docs/react-testing-library/setup#custom-render
+ * @param component the component under test
+ * @param options customized test options
+ */
+const render = (
+  ui: UI,
+  { wrapper: Wrapper = ChildrenPassthrough, ...options }: TestOptions = {},
+): RenderResult => {
+  return rtlRender(
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <GlobalStyle />
+      <Wrapper>{ui}</Wrapper>
+    </ThemeProvider>,
+    options,
+  )
+}
 
 export * from "@testing-library/react"
 
@@ -33,9 +83,19 @@ export {
 
 export { default as userEvent } from "@testing-library/user-event"
 
-export { customRender as render }
-
-export * from "jest-axe"
+export { render }
 
 export const escape = (ui: HTMLElement) =>
   fireEvent.keyDown(ui, { key: "Escape", keyCode: 27 })
+
+export const testA11y = async (
+  ui: UI,
+  options?: TestOptions,
+  axeOptions?: RunOptions,
+) => {
+  const { container } = render(ui, options)
+
+  const results = await axe(container, axeOptions)
+
+  expect(results).toHaveNoViolations()
+}
